31cb4a327a07b0af5c24b6c4ebe0dff45c317096
Catherine Farman <cfarman@gmail.com>
2012-07-09 19:58:20 -0400

    added Twitter project files

---
+++ Twitter/twitter_api.py
@@ -0,0 +1,44 @@
+import optparse
+import sys
+
+import twitter_functions
+
+def main(args):
+    parser = optparse.OptionParser("""Usage: %prog [-s <search term> | -t | -u <username>]""")
+
+    parser.add_option("-s", "--search",
+                      type="string",
+                      action="store",
+                      dest="search_term",
+                      default=None,
+                      help="Display tweets containing a particular string.")
+    parser.add_option("-t", "--trending-topics",
+                      action="store_true",
+                      dest="trending_topics",
+                      default=False,
+                      help="Display the trending topics.")
+    parser.add_option("-u", "--user-tweets",
+                      type="string",
+                      action="store",
+                      dest="user_tweets",
+                      default=False,
+                      help="Display a user's tweets.")
+    parser.add_option("-w", "--trending-tweets",
+                      action="store_true",
+                      dest="trending_tweets",
+                      default=False,
+                      help="Display the trending tweets.")
+
+    (opts, args) = parser.parse_args(args)
+
+    if opts.search_term:
+        twitter_functions.search(opts.search_term)
+    elif opts.trending_topics:
+        twitter_functions.trendingTopics()
+    elif opts.user_tweets:
+        twitter_functions.userTweets(opts.user_tweets)
+    elif opts.trending_tweets:
+	    twitter_functions.trendingTweets()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ Twitter/twitter_functions.py
@@ -0,0 +1,47 @@
+import twitter
+import util
+
+PHILA_WOEID = 2471217
+
+def search(searchTerm):
+    """
+    Print recent tweets containing `searchTerm`.
+    """
+    api = twitter.Api()
+    tweets = api.GetSearch(searchTerm)
+    for tweet in tweets:
+        util.safe_print(tweet.GetText())
+
+def trendingTopics():
+    """
+    Print the currently trending topics.
+    """
+    api = twitter.Api()
+
+    trending_topics = api.GetTrendsWoeid(PHILA_WOEID)
+    for topic in trending_topics:
+        util.safe_print(topic.name)
+
+def userTweets(username):
+    """
+    Print recent tweets by `username`.
+	pass #pass is a placeholder for where the code will go, so it doesn't break rest of code when run
+    """
+    api = twitter.Api()
+    user_tweets = api.GetUserTimeline(username)
+    for tweet in user_tweets:
+        util.safe_print(tweet.GetText())
+#    pass
+
+def trendingTweets():
+    """
+    Print tweets for all the trending topics.
+    """
+    api = twitter.Api()
+    trending_topics = api.GetTrendsWoeid(PHILA_WOEID)
+    for topic in trending_topics:
+        topicSearchTerm = topic.name
+        trending_tweets = api.GetSearch(topicSearchTerm)
+        for tweet in trending_tweets:
+            util.safe_print(tweet.GetText())
+    # pass
